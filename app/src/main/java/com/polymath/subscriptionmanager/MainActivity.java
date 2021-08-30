d1e2c8076c8131268e68bb850abaf27c80d32d21
Priyanshu Jain <priyanshujain2341@gmail.com>
2021-08-30 18:58:07 +0530

    Readme changes

--- app/src/main/java/com/polymath/subscriptionmanager/MainActivity.java
+++ app/src/main/java/com/polymath/subscriptionmanager/MainActivity.java
@@ -11,12 +11,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-<<<<<<< HEAD
-    protected void onCreate(Bundle savedInstanceState)
-    {
-=======
+
     protected void onCreate(Bundle savedInstanceState) {
->>>>>>> dfc86672c3f1165e61ff6d79b4976c8985bf6216
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -28,10 +24,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-<<<<<<< HEAD
-
-
-=======
->>>>>>> dfc86672c3f1165e61ff6d79b4976c8985bf6216
     }
 }
